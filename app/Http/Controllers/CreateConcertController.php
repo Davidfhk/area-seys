e7b1bb7e107ba0cb37b2bce31c4ba5d14752dcea
David Martos <davidignaciomartos@gmail.com>
2022-11-11 21:19:24 +0100

    remove excess code

--- app/Http/Controllers/CreateConcertController.php
+++ app/Http/Controllers/CreateConcertController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\TestResource;
 use Illuminate\Http\Request;
 use Src\Infrastructure\CreateConcertController as InfrastructureCreateConcertController;
 class CreateConcertController extends Controller
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Test::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
